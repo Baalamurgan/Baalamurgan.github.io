a7a089fa511835e95ceb065c62f2db30f3e3ca81
Baalamurgan <baalamurgan2307@gmail.com>
2021-07-20 09:32:42 +0000

    Deploying to master from @ Baalamurgan/Baalamurgan.github.io@7fb7a66e7cd15f1387975a8d56924624e6fb4e14 🚀

---
+++ 17007de1-e600248a65cd99a3f994.js
@@ -0,0 +1,2 @@
+(self.webpackChunkgatsby_starter_blog=self.webpackChunkgatsby_starter_blog||[]).push([[876],{5538:function(t,e,l){"use strict";l.d(e,{S1K:function(){return n}});var r=l(1046);function n(t){return(0,r.w_)({tag:"svg",attr:{viewBox:"0 0 16 16",fill:"currentColor"},child:[{tag:"path",attr:{fillRule:"evenodd",clipRule:"evenodd",d:"M7.116 8l-4.558 4.558.884.884L8 8.884l4.558 4.558.884-.884L8.884 8l4.558-4.558-.884-.884L8 7.116 3.442 2.558l-.884.884L7.116 8z"}}]})(t)}}}]);
+//# sourceMappingURL=17007de1-e600248a65cd99a3f994.js.map
--- src/data/ContactData.ts
+++
@@ -1,7 +0,0 @@
-const ContactData = [
-    "Baalamurgan K A",
-    "6F, BFC Krishna APTS, Choolaimedu, Chennai-94",
-    "7397346878",
-    "baalamurgan2307@gmail.com",
-];
-export { ContactData };
